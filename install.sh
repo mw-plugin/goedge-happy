2071fde178db99a3064c2785e3d1490086e0072d
Mr Chen <midoks@163.com>
2024-09-06 19:58:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ Install_App()
 		cd $GOEDGE_DIR && unzip ${FILE_TGZ}
 	fi
 
-	if [ -d $GOEDGE_DIR/edge-happy/edge-admin ];then
+	if [ -d $serverPath/goedge-happy ] && [ -d $GOEDGE_DIR/edge-happy/edge-admin ];then
 		cp -rf $GOEDGE_DIR/edge-happy/edge-admin/* $serverPath/goedge-happy/
 	fi
 
